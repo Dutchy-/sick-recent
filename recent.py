bd16e64bb6dfdeb6cc21b31e6df6b1836394fe9a
Edwin Smulders <edwin.smulders@gmail.com>
2012-04-13 01:31:35 +0200

    Fixed bug in date-checking Check if the downloaded file is newer than an existing file Delete old links from the directory

--- recent.py
+++ recent.py
@@ -7,9 +7,9 @@ import os.path
 # location of the recent series directory
 RECENT = '/home/dutchy/shares/mirrorpush'
 # days
-AIRED_WITHIN = '7'
+AIRED_WITHIN = 7
 # delete link after
-DELETE_AFTER = '14'
+DELETE_AFTER = 14
 
 def sickbeard_run():
 	# It passes 6 parameters to these scripts:
@@ -36,28 +36,41 @@ def sickbeard_run():
 
 def unlink():
 	# We need to do something about old links
+	for file in os.listdir(RECENT):
+		filepath = os.path.join(RECENT, file)
+		if not check_date(get_ctime(filepath), DELETE_AFTER):
+			print("Removing old link: {0}".format(file))
+			os.remove(filepath)
 	return
 
 def link(final, airdate):
 	print("Checking episode {0}".format(final))
-	if check_date(airdate):
+	if check_date(get_date(airdate)):
 		print("Air date {0} within {1} days, linking".format(airdate, AIRED_WITHIN))
 		linkname = os.path.join(RECENT, os.path.basename(final))
 		if not os.path.exists(linkname):
 			os.link(final, linkname)
 		else:
-			# TODO: check for propers?
-			pass
+			# perhaps insert .proper. in the filename?
+			if get_ctime(linkname) < get_ctime(final):
+				# The file is newer
+				print("We downloaded a newer file of the same episode, updating")
+				os.remove(linkname)
+				os.link(final,linkname)
 	else:
 		print("Air date {0} not within {1} days, not linking".format(airdate, AIRED_WITHIN))
-		# What do we do with older series?
-		
-
+		# What do we do with older episodes?
 	return
 
-def check_date(airdate):
-	delta =  datetime.datetime.now() - datetime.datetime.strptime(airdate, '%Y-%m-%d')
-	return delta.days < AIRED_WITHIN
+def get_ctime(file):
+	return datetime.datetime.fromtimestamp(os.path.getctime(file))
+
+def get_date(airdate):
+	return datetime.datetime.strptime(airdate, '%Y-%m-%d')
+
+def check_date(dt, d=AIRED_WITHIN):
+	delta =  datetime.datetime.now() - dt
+	return delta.days < d
 
 if __name__ == '__main__':
 	if len(sys.argv) == 7:
